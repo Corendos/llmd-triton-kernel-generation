90844ba44c38e095ed92a76d4f1e2a090c805da9
Tristan Gautie <tristan@zml.ai>
2026-02-23 13:36:15 +0000

    Small tweaks

--- generate_new_unified_attention.py
+++ generate_new_unified_attention.py
@@ -2,6 +2,9 @@ import os
 import torch
 from vllm_kernels.new_triton_unified_attention import unified_attention
 
+torch.set_printoptions(threshold=torch.inf)
+torch.manual_seed(0)
+
 os.environ["SHOULD_RUN_3D"] = "1"
 os.environ["TRITON_BACKEND_DEBUG"] = "1"
 os.environ["SHOULD_LOG"] = "1"
@@ -29,12 +32,9 @@ def generate_2D():
     key_cache = torch.randn(num_blocks, block_size, num_kv_heads, head_size , dtype=torch.bfloat16, device="cuda")
     value_cache = torch.randn(num_blocks, block_size, num_kv_heads, head_size, dtype=torch.bfloat16, device="cuda")
     block_tables = torch.full((batch_size, max_seq_len // block_size), 65535, dtype=torch.int32, device="cuda")
-    block_tables[:, 0] = 0
-    seq_lens = torch.full((batch_size,), 0, dtype=torch.int32, device="cuda")
-    seq_lens[0] = 8
-    start_loc = torch.full((batch_size+1,), 2, dtype=torch.int32, device="cuda")
-    start_loc[0] = 0
-    sliding_window = 0
+    block_tables[:, 0] = torch.arange(batch_size, dtype=torch.int32, device="cuda")
+    context_seq_lens = torch.full((batch_size,), 1, dtype=torch.int32, device="cuda")
+    start_loc = torch.arange(batch_size + 1, dtype=torch.int32, device="cuda")
 
     print("Parameters for unified_attention:")
     print(f"query shape: {query.shape}, dtype: {query.dtype}")
@@ -45,12 +45,11 @@ def generate_2D():
     print(f"value_cache shape: {value_cache.shape}, dtype: {value_cache.dtype}")
     print(f"block_tables shape: {block_tables.shape}, dtype: {block_tables.dtype}")
     print(f"start_loc shape: {start_loc.shape}, dtype: {start_loc.dtype}")
-    print(f"seq_lens shape: {seq_lens.shape}, dtype: {seq_lens.dtype}")
+    print(f"context_seq_lens shape: {context_seq_lens.shape}, dtype: {context_seq_lens.dtype}")
     print(f"max_input_len: {max_input_len}")
     print(f"k_scale: {k_scale}")
     print(f"v_scale: {v_scale}")
     print(f"alibi_slopes: {alibi_slopes}")
-    print(f"sliding_window: {sliding_window}")
     print(f"scale: {scale}")
 
     unified_attention(
@@ -60,7 +59,7 @@ def generate_2D():
         out=o,
         cu_seqlens_q=start_loc,
         max_seqlen_q=max_input_len,
-        seqused_k=seq_lens,
+        seqused_k=context_seq_lens,
         max_seqlen_k=max_seq_len,
         softmax_scale=scale,
         causal=True,
@@ -72,8 +71,7 @@ def generate_2D():
         v_descale=v_scale,
     )
 
-    print(o)
-
+    print(o[:,0,0])
 
 
 def generate_3D():
@@ -84,12 +82,9 @@ def generate_3D():
     key_cache = torch.randn(num_blocks, block_size, num_kv_heads, head_size , dtype=torch.bfloat16, device="cuda")
     value_cache = torch.randn(num_blocks, block_size, num_kv_heads, head_size, dtype=torch.bfloat16, device="cuda")
     block_tables = torch.full((batch_size, max_seq_len // block_size), 65535, dtype=torch.int32, device="cuda")
-    block_tables[:, 0] = 0
-    seq_lens = torch.full((batch_size,), 0, dtype=torch.int32, device="cuda")
-    seq_lens[0] = 8
-    start_loc = torch.full((batch_size+1,), 2, dtype=torch.int32, device="cuda")
-    start_loc[0] = 0
-    sliding_window = 0
+    block_tables[:, 0] = torch.arange(batch_size, dtype=torch.int32, device="cuda")
+    context_seq_lens = torch.full((batch_size,), 1, dtype=torch.int32, device="cuda")
+    start_loc = torch.arange(batch_size + 1, dtype=torch.int32, device="cuda")
 
     # Configure 3D kernel softmax buffers
     seq_threshold_3D = batch_size
@@ -119,12 +114,11 @@ def generate_3D():
     print(f"value_cache shape: {value_cache.shape}, dtype: {value_cache.dtype}")
     print(f"block_tables shape: {block_tables.shape}, dtype: {block_tables.dtype}")
     print(f"start_loc shape: {start_loc.shape}, dtype: {start_loc.dtype}")
-    print(f"seq_lens shape: {seq_lens.shape}, dtype: {seq_lens.dtype}")
+    print(f"context_seq_lens shape: {context_seq_lens.shape}, dtype: {context_seq_lens.dtype}")
     print(f"max_input_len: {max_input_len}")
     print(f"k_scale: {k_scale}")
     print(f"v_scale: {v_scale}")
     print(f"alibi_slopes: {alibi_slopes}")
-    print(f"sliding_window: {sliding_window}")
     print(f"scale: {scale}")
 
     unified_attention(
@@ -134,7 +128,7 @@ def generate_3D():
         out=o,
         cu_seqlens_q=start_loc,
         max_seqlen_q=max_input_len,
-        seqused_k=seq_lens,
+        seqused_k=context_seq_lens,
         max_seqlen_k=max_seq_len,
         softmax_scale=scale,
         causal=True,
@@ -151,7 +145,7 @@ def generate_3D():
         softmax_segm_expsum=softmax_segm_expsum,
     )
 
-    print(o)
+    print(o[:,0,0])
 
 
 if __name__ == "__main__":
